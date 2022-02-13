8fa8eb9afefa66546a7d9fa10d79ed4912e44217
21pages <pages21@163.com>
2022-02-13 20:20:35 +0800

    transplant tcp
    Signed-off-by: 21pages <pages21@163.com>

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,86 @@
 use hbb_common::{
     bytes::BytesMut, new_listener, protobuf::Message as _, rendezvous_proto::*, tokio,
-    udp::FramedSocket, Stream,
+    udp::FramedSocket, AddrMangle, Stream,
 };
+
+use log;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    // if cfg!(feature = "kcp") {
-    let mut socket = FramedSocket::new("0.0.0.0:22210").await.unwrap();
-    let mut listener = new_listener("0.0.0.0:22211", false).await.unwrap();
-    let mut rlistener = new_listener("0.0.0.0:22212", false).await.unwrap();
-    // } else {
-    //     let mut socket = FramedSocket::new("0.0.0.0:21116").await.unwrap();
-    //     let listener = new_listener("0.0.0.0:21116", false).await.unwrap();
-    //     let rlistener = new_listener("0.0.0.0:21117", false).await.unwrap();
-    // }
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
 
-    let mut id_map = std::collections::HashMap::new();
+    let mut socket = FramedSocket::new("0.0.0.0:21116").await.unwrap();
+    let mut listener = new_listener("0.0.0.0:21116", false).await.unwrap();
+    //<self_id, (self_addr, pk)>
+    let mut udp_map = std::collections::HashMap::<String, (std::net::SocketAddr, Vec<u8>)>::new();
+    //<peer_id, (self_addr stream)>
+    let mut tcp_map = std::collections::HashMap::<String, (std::net::SocketAddr, Stream)>::new();
     let relay_server = std::env::var("IP").unwrap();
-    let mut saved_stream = None;
     loop {
         tokio::select! {
             Some(Ok((bytes, addr))) = socket.next() => {
-                handle_udp(&mut socket, bytes, addr.into(), &mut id_map).await;
+                log::debug!("new udp socket from addr:{:?}", addr);
+                handle_udp(&mut socket, bytes, addr.into(), &mut udp_map).await;
             }
-            Ok((stream, addr)) = listener.accept() => {
+            Ok((stream, addr)) = listener.accept() => { //A tcp addr
+                log::info!("tcp listener accept new from addr:{:?}", addr);
                 let mut stream = Stream::from(stream, addr);
                 if let Some(Ok(bytes)) = stream.next_timeout(3000).await {
                     if let Ok(msg_in) = RendezvousMessage::parse_from_bytes(&bytes) {
                         match msg_in.union {
                             Some(rendezvous_message::Union::punch_hole_request(ph)) => {
-                                println!("punch_hole_request {:?}", addr);
-                                if let Some(addr) = id_map.get(&ph.id) {
-                                    let mut msg_out = RendezvousMessage::new();
-                                    msg_out.set_request_relay(RequestRelay {
-                                        relay_server: relay_server.clone(),
-                                        ..Default::default()
-                                    });
-                                    socket.send(&msg_out, addr.clone()).await.ok();
-                                    saved_stream = Some(stream);
-                                }
-                            }
-                            Some(rendezvous_message::Union::relay_response(_)) => {
-                                println!("relay_response {:?}", addr);
-                                let mut msg_out = RendezvousMessage::new();
-                                msg_out.set_relay_response(RelayResponse {
-                                    relay_server: relay_server.clone(),
-                                    ..Default::default()
-                                });
-                                if let Some(mut stream) = saved_stream.take() {
-                                    stream.send(&msg_out).await.ok();
-                                    if let Ok((stream_a, _)) = rlistener.accept().await {
-                                        let mut stream_a = Stream::from(stream_a, addr);
-                                        stream_a.next_timeout(3_000).await;
-                                        if let Ok((stream_b, _)) = rlistener.accept().await {
-                                            let mut stream_b = Stream::from(stream_b, addr);
-                                            stream_b.next_timeout(3_000).await;
-                                            relay(stream_a, stream_b, &mut socket, &mut id_map).await;
-                                        }
-                                    }
-                                }
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+                                log::info!("punch_hole_request {:?}, ph:{:?}", addr, ph);
+                                if let Some((peer_addr, _pk)) = udp_map.get(&ph.id) { //B udp addr
+                                    //给B发 udp punch request
+                                    let mut msg_udp = RendezvousMessage::new();
+                                    let send_ph = PunchHole {
+                                        socket_addr: AddrMangle::encode(addr.clone()), // A tcp addr
+                                         relay_server:relay_server.clone(),
+                                         nat_type: ph.nat_type.clone(),
+                                         ..Default::default()
+                                        };
+                                    msg_udp.set_punch_hole(send_ph.clone());
+                                    let ret = socket.send(&msg_udp, peer_addr.clone()).await.ok();
+                                    log::info!("send B {:?} PunchHole:{:?}, ret:{:?}", peer_addr, send_ph, ret);
 
-async fn relay(
-    stream: Stream,
-    peer: Stream,
-    socket: &mut FramedSocket,
-    id_map: &mut std::collections::HashMap<String, std::net::SocketAddr>,
-) {
-    let mut peer = peer;
-    let mut stream = stream;
-    peer.set_raw();
-    stream.set_raw();
-    loop {
-        tokio::select! {
-            Some(Ok((bytes, addr))) = socket.next() => {
-                handle_udp(socket, bytes, addr.into(), id_map).await;
+                                    tcp_map.insert(ph.id, (addr.clone(), stream));
+                                }
+                            }
+                            Some(rendezvous_message::Union::punch_hole_sent(phs)) => {
+                                log::info!("recv punch_hole_sent {:?} from {:?}", phs, addr);
+                                if let Some((_, stream)) = tcp_map.get_mut(&phs.id) {
+                                    //给A回 tcp punch response
+                                    if let Some((_, pk)) =  udp_map.get(&phs.id) {
+                                        let mut msg_tcp = RendezvousMessage::new();
+                                        let send_ph = PunchHoleResponse {
+                                            socket_addr: AddrMangle::encode(addr.clone()), //B tcp addr
+                                            pk: pk.clone(),
+                                            relay_server:relay_server.clone(),
+                                            ..Default::default()
+                                        };
+                                        msg_tcp.set_punch_hole_response(send_ph.clone());
+                                        let ret = stream.send(&msg_tcp).await.ok();
+                                        log::info!("send A {:?} PunchHoleResponse:{:?}, ret:{:?}", addr, send_ph, ret);
+                                    }
+                                } else {
+                                    log::warn!("no registered {:?}", phs.id);
+                                }
+
+                            }
+                            _ => {
+                                log::warn!("other rz {:?}", msg_in);
+                            }
+                        }
+                    } else {
+                        log::warn!("tcp listener read stream is not rz message from addr:{:?}", addr);
+                    }
+                } else {
+                    log::warn!("tcp listener read stream time out from addr:{:?}", addr);
+                }
             }
-            res = peer.next() => {
-                if let Some(Ok(bytes)) = res {
-                    stream.send_bytes(bytes.into()).await.ok();
-                } else {
-                    break;
-                }
-            },
-            res = stream.next() => {
-                if let Some(Ok(bytes)) = res {
-                    peer.send_bytes(bytes.into()).await.ok();
-                } else {
-                    break;
-                }
-            },
         }
     }
 }
@@ -106,19 +89,43 @@ async fn handle_udp(
     socket: &mut FramedSocket,
     bytes: BytesMut,
     addr: std::net::SocketAddr,
-    id_map: &mut std::collections::HashMap<String, std::net::SocketAddr>,
+    id_map: &mut std::collections::HashMap<String, (std::net::SocketAddr, Vec<u8>)>,
 ) {
     if let Ok(msg_in) = RendezvousMessage::parse_from_bytes(&bytes) {
+        log::debug!("udp rz bytes:{:?}", bytes);
         match msg_in.union {
             Some(rendezvous_message::Union::register_peer(rp)) => {
-                println!("register_peer {:?}", addr);
-                id_map.insert(rp.id, addr);
+                log::debug!("register_peer addr:{:?}, rp:{:?}", addr, rp);
+                let mut request_pk = true;
+                if id_map.contains_key(&rp.id) {
+                    let old = id_map.get(&rp.id).unwrap();
+                    let mut old_cloned = old.clone();
+                    old_cloned.0 = addr.clone();
+                    if old_cloned.1.len() > 0 {
+                        request_pk = false;
+                    }
+                    id_map.insert(rp.id.clone(), old_cloned);
+                } else {
+                    id_map.insert(rp.id.clone(), (addr, vec![]));
+                }
                 let mut msg_out = RendezvousMessage::new();
-                msg_out.set_register_peer_response(RegisterPeerResponse::new());
+                msg_out.set_register_peer_response(RegisterPeerResponse {
+                    request_pk,
+                    ..Default::default()
+                });
                 socket.send(&msg_out, addr).await.ok();
             }
-            Some(rendezvous_message::Union::register_pk(_)) => {
-                println!("register_pk {:?}", addr);
+            Some(rendezvous_message::Union::register_pk(pk)) => {
+                log::info!("register_pk addr:{:?}, pk:{:?}", addr, pk);
+                if id_map.contains_key(&pk.id) {
+                    let old = id_map.get(&pk.id).unwrap();
+                    let mut old_cloned = old.clone();
+                    old_cloned.1 = pk.pk.clone();
+                    id_map.insert(pk.id.clone(), old_cloned);
+                } else {
+                    id_map.insert(pk.id.clone(), (addr, vec![]));
+                }
+                log::info!("register pk: {:?}", id_map.get_key_value(&pk.id));
                 let mut msg_out = RendezvousMessage::new();
                 msg_out.set_register_pk_response(RegisterPkResponse {
                     result: register_pk_response::Result::OK.into(),
@@ -126,7 +133,11 @@ async fn handle_udp(
                 });
                 socket.send(&msg_out, addr).await.ok();
             }
-            _ => {}
+            _ => {
+                log::warn!("udp receive other msg_in.union:{:?}", msg_in.union);
+            }
         }
+    } else {
+        log::warn!("udp receive not RendezvousMessage:{:?}", bytes);
     }
 }
